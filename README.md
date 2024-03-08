f1f60e5572f1d9e3d9a8c53748bc83e330f5fa41
ZenitsuV <106746020+ZenitsuV@users.noreply.github.com>
2024-03-08 14:59:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-node-js-starters-express-middleware
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ZenitsuV/stackblitz-node-js-starters-express-middleware)
---
+++ index.js
@@ -0,0 +1,25 @@
+// run `node index.js` in the terminal
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<a href="/add-product" >Go to add product</a>');
+});
+
+app.listen(8000);
